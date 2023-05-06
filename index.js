ff8146ff9f27a732433aa38f7c105637c9c6a6b8
Emon <emon.mhk69@gmail.com>
2023-05-06 23:00:26 +0600

    database connection done

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express  = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -9,6 +11,37 @@ app.use(express.json());
 
 // user name: emonmhk69
 // password: syPend4NtBoipipl
+
+
+
+const uri = "mongodb+srv://emonmhk69:syPend4NtBoipipl@cluster0.zyyhzcl.mongodb.net/?retryWrites=true&w=majority";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+  
+}
+run().catch(console.dir);
+
 
 app.get("/" , (req, res)=>{
     res.send("simple croud API is running");
