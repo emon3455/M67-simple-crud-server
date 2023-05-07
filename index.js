09ecbd86111224d9c6a73ead68cc91c32723685f
Emon <emon.mhk69@gmail.com>
2023-05-07 09:17:33 +0600

    update done

--- index.js
+++ index.js
@@ -59,6 +59,25 @@ async function run() {
         res.send(result);
     })
 
+    app.put("/users/:id", async (req,res)=>{
+
+      const id  = req.params.id;
+      const user = req.body;
+
+      const filter = {_id: new ObjectId(id)}
+      const options = {upsert: true}
+
+      const updatedUser = {
+        $set:{
+          name: user.name,
+          email: user.email
+        }
+      }
+      const result = await userCollections.updateOne(filter, updatedUser, options);
+      res.send(result);
+
+    })
+
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
 
