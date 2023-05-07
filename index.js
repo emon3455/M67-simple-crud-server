43c3a555d65d510ccc91c13a459a2d0a5fde6537
Emon <emon.mhk69@gmail.com>
2023-05-07 08:52:23 +0600

    single info loaded from db by get method

--- index.js
+++ index.js
@@ -39,6 +39,13 @@ async function run() {
         res.send(result);
     });
 
+    app.get("/users/:id", async (req, res)=>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await userCollections.findOne(query);
+      res.send(result);
+    })
+
     app.post("/users", async(req, res)=>{
         const user = req.body;
         const result = await userCollections.insertOne(user);
